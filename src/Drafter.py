708862ebf0e1f8bf384a197536c1a9bcf6f4b07f
Eric <eapanthers@gmail.com>
2021-01-04 19:49:15 -0500

    Added setup.

---
+++ src/Drafter.py
@@ -0,0 +1,10 @@
+from Player import Player
+from PlayerList import PlayerList
+
+import csv
+import math
+from scipy.stats import chi2
+import sys
+import pandas as pd
+import numpy as np
+import random
