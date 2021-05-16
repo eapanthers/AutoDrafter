e7fff4c572066c759da6882d03980dfbdf6a7101
Eric <eapanthers@gmail.com>
2021-05-16 12:56:21 -0400

    Fixed formatting.

--- src/AutoDrafter.py
+++ src/AutoDrafter.py
@@ -6,8 +6,8 @@ import math
 
 WINDOW_X = 500
 WINDOW_Y = 500
-POPUP_X = int(WINDOW_X//1.5)
-POPUP_Y = int(WINDOW_Y//1)
+POPUP_X = int(WINDOW_X // 1.5)
+POPUP_Y = int(WINDOW_Y // 1)
 
 
 class Window(Frame):
@@ -57,8 +57,12 @@ class Window(Frame):
 
         main_menu.add_command(label="Run", command=self.run)
 
-        self.qb_label = Label(text="QB CSV:")  # TODO: Make viterbi dependent on these inputs
-        self.qb_label.place(x=0, y=0)  # TODO: Add fields to view and modify pick indices
+        self.qb_label = Label(
+            text="QB CSV:"
+        )  # TODO: Make viterbi dependent on these inputs
+        self.qb_label.place(
+            x=0, y=0
+        )  # TODO: Add fields to view and modify pick indices
         self.rb_label = Label(text="RB CSV:")
         self.rb_label.place(x=0, y=40)
         self.wr_label = Label(text="WR CSV:")
@@ -83,12 +87,21 @@ class Window(Frame):
         self.popup = Toplevel(self.master)
         self.popup.title("Configure")
         self.popup.geometry(f"{POPUP_X}x{POPUP_Y}")
-        config_selection = Checkbutton(self.popup, text="Load from config?", variable=self.config, onvalue=1, offvalue=0, command=self.fetch_config)
+        config_selection = Checkbutton(
+            self.popup,
+            text="Load from config?",
+            variable=self.config,
+            onvalue=1,
+            offvalue=0,
+            command=self.fetch_config,
+        )
         self.config_info_label = Label(self.popup, text="")
         pick_index_label = Label(self.popup, text="Pick index:")
         round_label = Label(self.popup, text="Number of rounds: ")
         teams_label = Label(self.popup, text="Number of teams: ")
-        qbs_label = Label(self.popup, text="Weight for QB selection (recommended - 1): ")
+        qbs_label = Label(
+            self.popup, text="Weight for QB selection (recommended - 1): "
+        )
         self.rbs_label = Label(self.popup, text="Weight for RB selection: ")
         self.wrs_label = Label(self.popup, text="Weight for WR selection: ")
         self.tes_label = Label(self.popup, text="Weight for TE selection: ")
@@ -107,8 +120,12 @@ class Window(Frame):
         self.e7 = Entry(self.popup)
         self.e8 = Entry(self.popup)
 
-        ppr_button = Radiobutton(self.popup, text="PPR", var=self.type, value=1, command=self.set_type)
-        standard_button = Radiobutton(self.popup, text="Standard", var=self.type, value=2, command=self.set_type)
+        ppr_button = Radiobutton(
+            self.popup, text="PPR", var=self.type, value=1, command=self.set_type
+        )
+        standard_button = Radiobutton(
+            self.popup, text="Standard", var=self.type, value=2, command=self.set_type
+        )
         done_button = Button(self.popup, text="Done", command=self.submit_config)
         cancel_button = Button(self.popup, text="Cancel", command=self.popup.destroy)
 
@@ -137,12 +154,24 @@ class Window(Frame):
         cancel_button.pack(side=RIGHT, padx=POPUP_X / 6)
 
     def run(self):
-        picks = Drafter.generate_picks(int(self.pick_index), int(self.num_rounds), int(self.num_teams))
-        players = Drafter.ff_viterbi(int(self.num_qbs), int(self.num_rbs), int(self.num_wrs), int(self.num_tes), picks, self.league_type, int(self.randomness))
+        picks = Drafter.generate_picks(
+            int(self.pick_index), int(self.num_rounds), int(self.num_teams)
+        )
+        players = Drafter.ff_viterbi(
+            int(self.num_qbs),
+            int(self.num_rbs),
+            int(self.num_wrs),
+            int(self.num_tes),
+            picks,
+            self.league_type,
+            int(self.randomness),
+        )
         if len(players) != len(picks):
             raise IndexError("Mismatched pick and player amounts.")
         for idx, pick in enumerate(picks):
-            print(f"Your optimal selection for Round {idx+1}, Pick {pick}: {players[idx].name} (ADP: {players[idx].adp}, Projected Points: {players[idx].proj_points})")
+            print(
+                f"Your optimal selection for Round {idx+1}, Pick {pick}: {players[idx].name} (ADP: {players[idx].adp}, Projected Points: {players[idx].proj_points})"
+            )
 
     def set_type(self):
         if self.type.get() == 1:
@@ -198,13 +227,17 @@ class Window(Frame):
                     elif self.league_type.lower() == "ppr":
                         self.type.set(1)
                     else:
-                        raise KeyError(f"League type '{self.league_type}' invalid, should be either ppr or standard")
+                        raise KeyError(
+                            f"League type '{self.league_type}' invalid, should be either ppr or standard"
+                        )
 
                     self.e8.delete(0, END)
                     self.e8.insert(0, self.conf_info["randomness"])
                     self.update_config_labels()
                 except KeyError as e:
-                    self.conf_message = f"Error parsing config: {e}, check that correct labels are used"
+                    self.conf_message = (
+                        f"Error parsing config: {e}, check that correct labels are used"
+                    )
                     self.update_config_labels()
             else:
                 self.conf_message = "Failed to load config - check file type."
@@ -213,16 +246,25 @@ class Window(Frame):
 
     def update_config_labels(self, *args):
         try:
-            self.config_info_label.configure(text=self.conf_message, wraplength=POPUP_X, justify=CENTER)
+            self.config_info_label.configure(
+                text=self.conf_message, wraplength=POPUP_X, justify=CENTER
+            )
             try:
                 self.rbs_label.configure(
-                    text=f"Weight for RB selection: (recommended - {math.ceil((int(self.round_var.get()) - 2 - int(self.qb_var.get())) / 2)})", wraplength=POPUP_X, justify=CENTER)
+                    text=f"Weight for RB selection: (recommended - {math.ceil((int(self.round_var.get()) - 2 - int(self.qb_var.get())) / 2)})",
+                    wraplength=POPUP_X,
+                    justify=CENTER,
+                )
                 self.wrs_label.configure(
                     text=f"Weight for WR selection: (recommended - {math.floor((int(self.round_var.get()) - 2 - int(self.qb_var.get())) // 2)})",
-                    wraplength=POPUP_X, justify=CENTER)
+                    wraplength=POPUP_X,
+                    justify=CENTER,
+                )
                 self.tes_label.configure(
                     text=f"Weight for TE selection: (recommended - {int(self.round_var.get()) - int(self.qb_var.get()) - int(self.wr_var.get()) - int(self.rb_var.get())})",
-                    wraplength=POPUP_X, justify=CENTER)
+                    wraplength=POPUP_X,
+                    justify=CENTER,
+                )
             except ValueError:
                 pass
             self.after(1000, self.update_config_labels)
